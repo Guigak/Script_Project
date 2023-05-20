dc061203494605035aa169e1af0fcc09fd220954
Guigak <97238914+Guigak@users.noreply.github.com>
2023-05-20 19:58:28 +0900

    Create HAMBOGOGA class
    just create class and test tkinter

---
+++ HAMBOGOGA.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import font
+
+class HAMBOGOGA :
+    def __init__(self) :
+        self.window_main = Tk()
+        self.window_main.geometry("600x900")
+
+        self.window_main.mainloop()
+
+HAMBOGOGA()
