c32b4ee454c128d2437865036ad9adb899f37890
Guigak <97238914+Guigak@users.noreply.github.com>
2023-05-20 20:08:18 +0900

    Create InitAppTitle function
    just create and test about size and location

--- HAMBOGOGA.py
+++ HAMBOGOGA.py
@@ -6,6 +6,13 @@ class HAMBOGOGA :
         self.window_main = Tk()
         self.window_main.geometry("600x900")
 
+        # Init
+        self.InitAppTitle()
+
         self.window_main.mainloop()
+
+    def InitAppTitle(self) :
+        self.button_AppTitle = Button(self.window_main, text= "HAMBOGOGA", width= 70, height= 2)
+        self.button_AppTitle.place(x= 15, y= 15)
 
 HAMBOGOGA()
