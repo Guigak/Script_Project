bdae801901d47311aef646cbb2f6c5749b152040
Guigak <97238914+Guigak@users.noreply.github.com>
2023-05-20 20:24:52 +0900

    Create InitLocationListBox function
    just create listbox and test size and location

--- HAMBOGOGA.py
+++ HAMBOGOGA.py
@@ -8,11 +8,19 @@ class HAMBOGOGA :
 
         # Init
         self.InitAppTitle()
+        self.InitLocationListBox()
 
         self.window_main.mainloop()
 
     def InitAppTitle(self) :
-        self.button_AppTitle = Button(self.window_main, text= "HAMBOGOGA", width= 70, height= 2)
+        self.button_AppTitle = Button(self.window_main, text= "HAMBOGOGA", width= 70, height= 2)    # height 1 : 25?
         self.button_AppTitle.place(x= 15, y= 15)
+
+    def InitLocationListBox(self) :
+        self.scrollbar_Location = Scrollbar(self.window_main)
+        self.scrollbar_Location.place(x=290, y=80)
+
+        self.listbox_Location = Listbox(self.window_main, width= 33, height= 10, yscrollcommand= self.scrollbar_Location.set)
+        self.listbox_Location.place(x= 15, y= 80)
 
 HAMBOGOGA()
