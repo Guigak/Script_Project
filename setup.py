592540513ec23b2b6438371c4c8c16b95d4254b4
Guigak <97238914+Guigak@users.noreply.github.com>
2023-06-08 23:52:21 +0900

    create setup.py for distutils
    create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name= "HAMBOGOGA",
+    version= '1.0',
+    py_modules= ['HAMBOGOGA', 'noti', 'cpptime'],
+    data_files= ['LocalData.xlsx', 'title_logo.png']
+)
